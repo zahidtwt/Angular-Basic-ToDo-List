d2da6ab9cf8989f968943530bd0ecf4ba59b4405
Zahid Ul Islam <zahidtwt@gmail.com>
2023-03-26 16:13:27 +0600

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
-
+import {Todo} from './model'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'todo-app';
+  todoList: Todo [] = [];
+  todo: Todo = {
+    title : '',
+    id : null
+  }
+  addTodo():void {
+    if(this.todo.id) {
+      // update
+      this.todoList = this.todoList.map(obj => {
+        if(obj.id === this.todo.id){
+          obj.title = this.todo.title;
+        }
+        return obj;
+
+      })
+      this.todo = {
+        title: '',
+        id: null
+      }
+
+    } else {
+      // create logic
+      this.todo.id = Date.now(); // setting id
+      this.todoList.push({...this.todo}); // pushing todo item in the to todolist array
+      this.todo = {
+        title: '',
+        id: null
+      }
+  }
+  }
+  editTodo(todo:Todo):void {
+    this.todo = {...todo};
+  }
+  deleteTodo(id:number):void{
+    this.todoList = this.todoList.filter(obj => obj.id !==id)
+  }
 }
